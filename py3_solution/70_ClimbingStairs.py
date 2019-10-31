55b994fae2e96597cca18b729e6cbe224c48f960
GPH <gphsmail@163.com>
2019-10-31 11:12:07 +0800

    Create 70_ClimbingStairs.py

---
+++ py3_solution/70_ClimbingStairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        if n == 0:
+            return 1
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            dp[0] = 1
+            dp[1] = 1
+            dp[2] = 2
+            
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+            
+        return dp[n]
